af506bde4d78beb98fe2d34a95ded67390fd1361
Michael Mosmann <michael.mosmann@blau.de>
2016-08-12 14:38:13 +0200

    plugin executed

--- maven-plugin/src/main/java/de/flapdoodle/codedoc/maven/CodeDocMojo.java
+++ maven-plugin/src/main/java/de/flapdoodle/codedoc/maven/CodeDocMojo.java
@@ -16,18 +16,61 @@
  */
 package de.flapdoodle.codedoc.maven;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
-@Mojo(name = "codedoc", defaultPhase = LifecyclePhase.PREPARE_PACKAGE)
+@Mojo( name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
 public class CodeDocMojo extends AbstractMojo {
 
-	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		getLog().info( "---------------CodeDoc Mojo---------------" );
-	}
+    /**
+     * Location of the file.
+     */
+    @Parameter( defaultValue = "${project.build.directory}", property = "outputDir", required = true )
+    private File outputDirectory;
 
+    public void execute()
+        throws MojoExecutionException
+    {
+        File f = outputDirectory;
+
+        if ( !f.exists() )
+        {
+            f.mkdirs();
+        }
+
+        File touch = new File( f, "touch.txt" );
+
+        FileWriter w = null;
+        try
+        {
+            w = new FileWriter( touch );
+
+            w.write( "touch.txt" );
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Error creating file " + touch, e );
+        }
+        finally
+        {
+            if ( w != null )
+            {
+                try
+                {
+                    w.close();
+                }
+                catch ( IOException e )
+                {
+                    // ignore
+                }
+            }
+        }
+    }
 }
