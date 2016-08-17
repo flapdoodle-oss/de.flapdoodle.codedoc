0f657663e0753221332291d9f88fbf5df710f425
Michael Mosmann <michael@mosmann.de>
2016-08-17 06:46:45 +0200

    refactor

--- base/src/main/java/de/flapdoodle/codedoc/Reference.java
+++ base/src/main/java/de/flapdoodle/codedoc/Reference.java
@@ -22,20 +22,28 @@ import java.util.regex.Pattern;
 import org.immutables.value.Value;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
 
 @Value.Immutable
 public abstract class Reference {
 	
-	private static Pattern PACKAGE_AND_CLASS_PATTERN=Pattern.compile("^(?<package>([a-z0-9]+\\.)+)(?<class>[A-Z][A-Za-z0-9]+)(\\.(?<constructor>\\k<class>))?(\\.(?<method>[A-Za-z0-9]+))?");
-	private static Pattern METHOD_ARGS_PATTERN=Pattern.compile("\\("
+	private static final Pattern PACKAGE_AND_CLASS_PATTERN=Pattern.compile("^(?<package>([a-z0-9]+\\.)+)(?<class>[A-Z][A-Za-z0-9]+)(\\.(?<constructor>\\k<class>))?(\\.(?<method>[A-Za-z0-9]+))?");
+	private static final Pattern METHOD_ARGS_PATTERN=Pattern.compile("\\("
 			+ "(?<body>.*)"
 			+ "\\)");
-//	private static String METHOD_ARGS="";
-//	private static Pattern METHOD_REF=Pattern.compile("\\.(?<methodname>([a-z].*))\\("+METHOD_ARGS+"\\)");
-//	private static Pattern CONSTRUCTOR_REF=Pattern.compile("\\.(?<methodname>([A-Z].*))\\("+METHOD_ARGS+"\\)");
+	private static final Pattern METHOD_ARG_PATTERN=Pattern.compile("\\s?(?<arg>[^,.]+)\\s?,?\\s?");
 	
 	public abstract String className();
 	public abstract Optional<String> method();
+	
+	public static abstract class WithArgumentTypes {
+		public abstract ImmutableList<String> arguments();
+	}
+	
+	@Value.Immutable
+	public static abstract class Constructor extends WithArgumentTypes {
+		
+	}
 	
 	public static Reference parse(String src) {
 		int lastDot=src.lastIndexOf('.');
@@ -54,6 +62,7 @@ public abstract class Reference {
 				Matcher methodMatcher = METHOD_ARGS_PATTERN.matcher(left);
 				if (methodMatcher.matches()) {
 					System.out.println("method body: "+methodMatcher.group("body"));
+					System.out.println("args: "+methodArgs(methodMatcher.group("body")));
 				}
 			} else {
 				System.out.println("unknown: "+src);
@@ -61,5 +70,24 @@ public abstract class Reference {
 		}
 		return ImmutableReference.builder()
 				.build();
+	}
+	
+	private static ImmutableList<String> methodArgs(String group) {
+		ImmutableList.Builder<String> builder=ImmutableList.builder();
+		
+		String left=group;
+		do {
+			Matcher matcher = METHOD_ARG_PATTERN.matcher(left);
+			if (matcher.find()) {
+//				System.out.println("Arg: "+matcher.group("arg"));
+				builder.add(matcher.group("arg"));
+				left=left.substring(matcher.end());
+			} else {
+				System.out.println("end");
+				left="";
+			}
+		} while (!left.isEmpty());
+		
+		return builder.build();
 	}
 }
